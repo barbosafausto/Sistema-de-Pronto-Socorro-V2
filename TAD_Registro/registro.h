bc58a7a6ca15ffebec84e6dd3ded9be4bca3a710
faustus <vitalbfausto@gmail.com>
2025-11-17 13:55:52 -0300

    Struct registro finalizada.

--- TAD_Registro/registro.h
+++ TAD_Registro/registro.h
@@ -9,28 +9,23 @@
   **/
   
   typedef struct registro_ REGISTRO;
-  typedef struct no_registro NOr;
+  typedef struct no_registro NO;
   
   REGISTRO* registro_criar(void);
-  NOr* no_criar(PACIENTE* p);
   
   //Não é necessário "bool registro_cheio()" pois em teoria não existe limite para o número de pacientes que podem estar registrados no hospital.
   bool registro_vazio(REGISTRO* r);
-  PACIENTE* no_get_paciente(NOr* n);
   
   //"registro_buscar()" busca pelo paciente no registro. "registro_verificar()" e "registro_recuperar()" chamam "registro_buscar()", suas únicas diferenças sendo o retorno das funções.
-  NOr* registro_buscar(REGISTRO* r, int id); //*
-  bool registro_verificar(REGISTRO* r, int id); //*
+  char registro_verificar(REGISTRO* r, int id); //*
   PACIENTE* registro_recuperar(REGISTRO* r, int id); //*
   
-  bool registro_inserir(REGISTRO* r, NOr* n); //*
-  NOr* registro_remover(REGISTRO* r, int id); //*
+  bool registro_inserir(REGISTRO* r, PACIENTE* p); //*
+  PACIENTE* registro_remover(REGISTRO* r, int id); //*
   
   void registro_listar(REGISTRO* r); //*
-  bool registro_salvar(REGISTRO* r); //*
+  bool registro_salvar(REGISTRO** r); //*
   REGISTRO* registro_carregar(void); //*
-  
-  bool no_apagar(NOr** n);
   
   //FUNÇÃO TEMPORÁRIA PARA EVITAR VAZAMENTO DE MEMÓRIA DURANTE OS TESTES. Esta função NÃO libera os nós dentro do registro e será substituída por "registro_salvar()" futuramente.
   bool registro_apagar(REGISTRO** r);
--- TAD_Registro/testes.c
+++ TAD_Registro/testes.c
@@ -9,18 +9,12 @@ int main(void){
   printf("%s\n", paciente_get_nome(p));
   printf("%d\n", paciente_get_id(p));
     
-  NOr* n = no_criar(p);
-  printf("%s\n", paciente_get_nome(no_get_paciente(n)));
-  printf("%d\n", paciente_get_id(no_get_paciente(n)));
-    
-  no_apagar(&n);
-  if (n == NULL) printf("Nó apagado.\n");
-  p = NULL; //O ponteiro para paciente dentro do nó não é o mesmo que esta variável "p".
-  
   REGISTRO* r = registro_criar();
   if (r != NULL) printf("Registro criado.\n");
   registro_apagar(&r);
   if (r == NULL) printf("Registro apagado.\n");
-  
+
+  paciente_apagar(&p);
+   
   return 0;
 }
